2d1b853e190157d641c34cd1cdeec235bbd29581
Vladyslav1992 <vladpleten@gmail.com>
2019-02-19 23:33:48 +0200

    update page object structure

---
+++ AutomationFramework/Pages/LoginScenarioPost.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace AutomationFramework.Pages
+{
+    class LoginScenarioPost
+    {
+        public LoginScenarioPost()
+        {
+            PageFactory.InitElements(DriverDefinition.Driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "input[name='userid']")]
+        public IWebElement UserNameField { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "input[name^='pass']")]
+        public IWebElement PasswordField { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "input[name*='repeat']")]
+        public IWebElement RepeatPasswordField { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "input[value='LOGIN']")]
+        public IWebElement LoginButton { get; set; }
+    }
+}
---
+++ AutomationFramework/Pages/TestScenariosPage.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium.Support.PageObjects;
+
+namespace AutomationFramework.Pages
+{
+    class TestScenariosPage
+    {
+        public TestScenariosPage()
+        {
+            PageFactory.InitElements(DriverDefinition.Driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = )]
+    }
+}
